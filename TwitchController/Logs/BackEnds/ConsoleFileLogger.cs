3031cf69c12f0a0b0c8a3f864648633ddd216a35
Roxandi <kosty2004askzvwe@gmail.com>
2025-02-22 22:45:44 +0700

    Added loggers (skill issue) to dev branch

---
+++ TwitchController/Logs/BackEnds/ConsoleFileLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchController.Logs.BackEnds
+{
+    internal class ConsoleFileLogger : ILogger
+    {
+        private string _filepath;
+
+        public ConsoleFileLogger()
+        {
+            var dir = Directory.GetCurrentDirectory() + "\\Logs";
+            Directory.CreateDirectory(dir);
+            _filepath = dir + "\\" + DateTime.Now.ToString()
+                .Replace(" ", string.Empty)
+                .Replace(":", ".") + ".txt";
+            File.Create(_filepath).Close();
+        }
+
+        public void Error(string message, string? err = null)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine(_filepath, $"[ERR] {message} {err ?? ""}.");
+            writer.Close();
+            Console.WriteLine($"[ERR] {message} {err ?? ""}.");
+        }
+
+        public void External(string type, string name, string message, string? err = null)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[{name}:{type}] {message} {err ?? ""}.");
+            writer.Close();
+            Console.WriteLine($"[{name}:{type}] {message} {err ?? ""}.");
+        }
+
+        public void Info(string message)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[INFO] {message}.");
+            writer.Close();
+            Console.WriteLine($"[INFO] {message}.");
+        }
+
+        public void Warn(string message)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[WARN] {message}.");
+            writer.Close();
+            Console.WriteLine($"[WARN] {message}.");
+        }
+    }
+}
---
+++ TwitchController/Logs/BackEnds/FileLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchController.Logs.BackEnds
+{
+    internal class FileLogger : ILogger
+    {
+        private string _filepath;
+
+        public FileLogger() {
+            var dir = Directory.GetCurrentDirectory() + "\\Logs";
+            Directory.CreateDirectory(dir);
+            _filepath = dir + "\\" + DateTime.Now.ToString()
+                .Replace(" ", string.Empty)
+                .Replace(":", ".") + ".txt";
+            File.Create(_filepath).Close();
+        }
+
+        public void Error(string message, string? err = null)
+        {
+            var writer = File.AppendText(_filepath); 
+            writer.WriteLine(_filepath, $"[ERR] {message} {err ?? ""}.");
+            writer.Close();
+        }
+
+        public void External(string type, string name, string message, string? err = null)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[{name}:{type}] {message} {err ?? ""}.");
+            writer.Close();
+        }
+
+        public void Info(string message)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[INFO] {message}.");
+            writer.Close();
+        }
+
+        public void Warn(string message)
+        {
+            var writer = File.AppendText(_filepath);
+            writer.WriteLine($"[WARN] {message}.");
+            writer.Close();
+        }
+    }
+}
